e7ebbfeae63b463e6dcda282bc9e4521d25cdcc8
lodhaakash07 <lodhaakash07@gmail.com>
2014-08-16 17:40:24 +0530

    Part 1 solution

--- R-Programming/ProgrammingAssignment1/pollutantmean.R
+++ R-Programming/ProgrammingAssignment1/pollutantmean.R
@@ -29,11 +29,12 @@ pollutantmean <- function(directory, pollutant, id = 1:332) {
   # GEt all the pollutant data from all the id
   a <- sapply(paste("./",directory,"/",files[id],sep=""),dd,pollutant)
   names(a) <- NULL
-  data <- vector()
-  for( i in a) {
-    data <- c(data,i)
-  }
- mean(data)
+  
+   data <- vector()
+   for( i in a) {
+     data <- c(data,i)
+   }
+   mean(data)
  # mean(poll,na.rm=T)
   ## Return the mean of the pollutant across all monitors list
   ## in the 'id' vector (ignoring NA values)
