590676b505971a6f9a3d8e8e66f7d7be504159d1
lodhaakash07 <lodhaakash07@gmail.com>
2014-08-23 12:52:37 +0530

    DataSet for the Peer Assignment

---
+++ Exploratory Data ANalysis/q1.R
@@ -0,0 +1,84 @@
+# Akash Lodha
+# Data Science Specialisation - Coursera
+# Course 4 - Exploratory Data Analysis
+
+# Quiz 1
+
+
+# Question 1
+# Which of the following is a principle of analytic graphics?
+    # Only do what your tools allow you to do
+    # Don't plot more than two variables at at time
+    # Show box plots (univariate summaries)
+    # Show multivariate data - ANS
+    # Make judicious use of color in your scatterplots
+
+
+# Question 2
+# What is the role of exploratory graphs in data analysis?
+    # They are used in place of formal modeling.
+    # They are typically made very quickly. - ANS
+    # Axes, legends, and other details are clean and exactly detailed.
+    # They are made for formal presentations.
+
+# Question 3
+# Which of the following is true about the base plotting system?
+    # The system is most useful for conditioning plots
+    # Plots are created and annotated with separate functions -aNS
+    # Plots are typically created with a single function call
+    # Margins and spacings are adjusted automatically depending on the type of plot and the data'
+
+
+# Question 4
+# Which of the following is an example of a valid graphics device in R?
+    # A PDF file -ANS
+    # A file folder
+    # The keyboard
+    # A socket connection
+
+
+# Question 5
+# Which of the following is an example of a vector graphics device in R?
+    # JPEG
+    # SVG -ANS
+    # PNG
+    # GIF
+
+
+# Question 6
+# Bitmapped file formats can be most useful for
+    # Plots that are not scaled to a specific resolution 
+    # Plots that require animation or interactivity
+    # Scatterplots with many many points
+    # Plots that may need to be resized -ANS
+
+
+# Question 7
+# Which of the following functions is typically used to add elements to a plot in the base graphics system?
+    # hist()
+    # plot()
+    # boxplot()
+    # lines() -ANS
+
+# Question 8
+# Which function opens the screen graphics device for the Mac?
+    # bitmap()
+    # png()
+    # quartz() 0ANS
+    # pdf()
+
+
+# Question 9
+# What does the 'pch' option to par() control?
+# the plotting symbol/character in the base graphics system -ANS
+# the size of the plotting symbol in a scatterplot
+# the line width in the base graphics system
+# the orientation of the axis labels on the plot
+
+
+# Question 10
+# If I want to save a plot to a PDF file, which of the following is a correct way of doing that?
+    # Open the PostScript device with postscript(), construct the plot, then close the device with dev.off().
+    # Open the screen device with quartz(), construct the plot, and then close the device with dev.off().
+    # Construct the plot on the screen device and then copy it to a PDF file with dev.copy2pdf()  -ANS
+    # Construct the plot on the PNG device with png(), then copy it to a PDF with dev.copy2pdf()
